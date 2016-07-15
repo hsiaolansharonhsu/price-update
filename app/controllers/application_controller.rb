4727ab18b0eeb584ee45ef5150fc8cd967e474b8
Sharon Hsu <hsiaolanhsu@yahoo.com>
2016-07-15 17:37:20 -0400

    fixed hound vialations

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,7 @@ class ApplicationController < ActionController::Base
 
     protected
 
-    def configure_permitted_parameters
-      devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
-    end
-
+  def configure_permitted_parameters
+    devise_parameter_sanitizer.for(:sign_up) << [:first_name, :last_name]
+  end
 end
