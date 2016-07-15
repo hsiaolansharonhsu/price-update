6ca493d61319b364ee5d201b7efa5433818ff427
Sharon Hsu <hsiaolanhsu@yahoo.com>
2016-07-15 16:47:12 -0400

    fixed hound vialations

--- db/schema.rb
+++ db/schema.rb
@@ -14,7 +14,7 @@
 ActiveRecord::Schema.define(version: 20160715003133) do
 
   # These are extensions that must be enabled in order to support this database
-  enable_extension "plpgsql"
+  enable_extension 'plpgsql'
 
   create_table "users", force: :cascade do |t|
     t.string   "first_name",                          null: false
