983c150a42add018af2116e463337ffc89992fdb
Sharon Hsu <hsiaolanhsu@yahoo.com>
2016-07-15 12:09:38 -0400

    add coverall gem modified .gitigore and spec-helper accordingly

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -96,6 +96,8 @@ RSpec.configure do |config|
   # as the one that triggered the failure.
   Kernel.srand config.seed
 =end
+require 'coveralls'
+Coveralls.wear!('rails')
 end
 # require 'coveralls'
 # Coveralls.wear!
