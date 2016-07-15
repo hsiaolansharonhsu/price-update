af7b5f73560de78f237925cc48cbb5f9ad980111
Hsiaolan Sharon Hsu <hsiaolanhsu@yahoo.com>
2016-07-14 22:20:51 -0400

    add badges

--- spec/factories/users.rb
+++ spec/factories/users.rb
@@ -1,11 +1,10 @@
 FactoryGirl.define do
-  # factory :user do
-  #   sequence(:email) { |n| "bg#{n}@pipedpiper.net" }
-  # factory :user do
-  #   first_name "Bertram"
-  #   last_name "Guilfoyle"
-  #   email
-  #   password "12345password"
-  #   password_confirmation "12345password"
-  # end
+  sequence(:email) { |n| "bg#{n}@pipedpiper.net" }
+  factory :user do
+    first_name "Bertram"
+    last_name "Guilfoyle"
+    email
+    password "12345password"
+    password_confirmation "12345password"
+  end
 end
